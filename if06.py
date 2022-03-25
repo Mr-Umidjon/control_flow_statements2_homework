eaa7059ecdbf696db9e92443fdb85b016142999c
Umidjon <mrumidjon2020@gmail.com>
2022-03-25 14:53:36 +0500

    Find the index of the largest digit of the five-digit number. 02

--- if06.py
+++ if06.py
@@ -6,32 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    x1 = n % 10
-    n //= 10
+    n = str(n)
 
-    x2 = n % 10
-    n //= 10
-
-    x3 = n % 10
-    n //= 10
-
-    x4 = n % 10
-    n //= 10
-
-    x5 = n % 10
-    n //= 10
-
-    max_index = 1
-    maximum = x1
-    if maximum < x2:
+    max_index = 0
+    maximum = n[0]
+    if maximum < n[1]:
+        max_index = 1
+    if maximum < n[2]:
         max_index = 2
-    if maximum < x3:
+    if maximum < n[3]:
         max_index = 3
-    if maximum < x4:
+    if maximum < n[4]:
         max_index = 4
-    if maximum < x5:
-        max_index = 5
-    return max_index
+    return max_index+1
 
 
 print(main(16325))
